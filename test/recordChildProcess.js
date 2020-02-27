1ff9fa9e8f7980a6da6f9ba46735381a2403ba3a
Benjie Gillam <benjie@jemjie.com>
2020-02-27 09:41:46 +0000

    Fix intermittent CI errors on Mac (#3)
    * Fix intermittent CI errors on Mac
    
    * Run CI on PR
    
    * Update snapshots: merge consecutive flushes to same pipe
    
    * Track stdout and stderr separately
    
    * Update Node version specific tests
    
    * Empty commit to test CI stability
    
    * Empty commit to test CI stability
    
    * Empty commit to test CI stability
    
    * Empty commit to test CI stability
    
    * Empty commit to test CI stability
    
    * Empty commit to test CI stability

--- test/recordChildProcess.js
+++ test/recordChildProcess.js
@@ -5,19 +5,27 @@
  * @kind function
  * @name recordChildProcess
  * @param {ChildProcess} childProcess Node.js child process.
- * @returns {Promise<{output: Array<Array<string, string>>, exitCode: number, signal: string}>} Resolves a multidimensional array of output types (`stderr` or `stdout`) and values, the exit code if the child exited on its own, or the signal by which the child process was terminated.
+ * @returns {Promise<{output: {stdout: string, stderr: string}, exitCode: number, signal: string}>} Resolves a multidimensional array of output types (`stderr` or `stdout`) and values, the exit code if the child exited on its own, or the signal by which the child process was terminated.
  * @ignore
  */
 module.exports = function recordChildProcess(childProcess) {
   return new Promise((resolve, reject) => {
-    const output = []
+    const output = {
+      stdout: '',
+      stderr: ''
+    }
+
+    const add = (pipe, data) => {
+      const text = data.toString()
+      output[pipe] += text
+    }
 
     childProcess.stdout.on('data', data => {
-      output.push(['stdout', data.toString()])
+      add('stdout', data)
     })
 
     childProcess.stderr.on('data', data => {
-      output.push(['stderr', data.toString()])
+      add('stderr', data)
     })
 
     childProcess.once('error', reject)
